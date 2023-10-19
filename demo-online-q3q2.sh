2aec8d108efb38980b3151e2c208035475382d56
m32 <grzegorz.makarewicz@gmail.com>
2023-10-20 01:39:05 +0200

    dalej

--- demo-online-q3q2.sh
+++ demo-online-q3q2.sh
@@ -4,5 +4,5 @@
 --user=firma1111111111 \
 --query=1 \
 --query-type=incremental \
---date-from=2023-10-20T09:00:00+02:00 \
---date-to=2023-10-20T10:00:00+02:00
+--date-from=2023-10-20T09:00:00Z \
+--date-to=2023-10-20T09:15:00Z
