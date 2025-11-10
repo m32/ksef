503542d6f8d02da5880b7c4a935c115bb343a65a
m32 <grzegorz.makarewicz@gmail.com>
2025-11-10 10:41:39 +0100

    xades z kluczem ecdsa

--- x-sign-endesive-xml.py
+++ x-sign-endesive-xml.py
@@ -4,7 +4,7 @@ from lxml import etree
 from cryptography import x509
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric import rsa, padding, ec
 from cryptography.hazmat.primitives.serialization import pkcs12
 from cryptography.x509.oid import NameOID
 
@@ -23,18 +23,29 @@ def main():
     fnamexml = sys.argv[2]
 
     p12pk, p12pc, p12oc = load_pfx(fnamekey+'.p12', '12345678')
-    assert isinstance(p12pk, rsa.RSAPrivateKey)
+
+    assert isinstance(p12pk, rsa.RSAPrivateKey) or isinstance(p12pk, ec.EllipticCurvePrivateKey)
     assert isinstance(p12pc, x509.Certificate)
+    if isinstance(p12pk, rsa.RSAPrivateKey):
+        signaturemethod = None
+    else:
+        signaturemethod = 'http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256'
 
     with open(fnamexml, "rb") as fp:
         data = fp.read()
 
     def signproc(tosign, algosig):
-        sig = p12pk.sign(
-            tosign,
-            padding.PKCS1v15(),
-            getattr(hashes, algosig.upper())(),
-        )
+        if isinstance(p12pk, rsa.RSAPrivateKey):
+            sig = p12pk.sign(
+                tosign,
+                padding.PKCS1v15(),
+                getattr(hashes, algosig.upper())(),
+            )
+        else:
+            sig = p12pk.sign(
+                tosign,
+                ec.ECDSA(getattr(hashes, algosig.upper())())
+            )
         return sig
 
     cert = p12pc
@@ -50,6 +61,7 @@ def main():
         signproc,
         False,
         True,
+        signaturemethod='http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256'
     )
     data = etree.tostring(doc, encoding="UTF-8", xml_declaration=True, standalone=False)
 
